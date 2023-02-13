11c58275272d1d8de5da28f07852f589338f0886
Zachary <zderish@gmail.com>
2023-02-13 16:48:15 -0500

    Add homeworld column in table and fix DOM error

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ charBtn.addEventListener("click", function(){
 let rowCount = 0
 
 function getChar(data){
-    // get movie and create header with title
+    // get movie and create header with movie title
     console.log(input.value)
     const movie = data.result[input.value-1].properties
     console.log(movie)
@@ -31,7 +31,7 @@ function getChar(data){
     header.innerHTML = movie.title
     row1.appendChild(header)
 
-    // get movie characters
+    // create table headers for name, birth year and homeworld
     let row2 = table.insertRow(rowCount)
     rowCount+=1
     let nameHeader = document.createElement("th")
@@ -40,7 +40,12 @@ function getChar(data){
     let birthHeader = document.createElement("th")
     birthHeader.innerHTML = "birth year"
     row2.appendChild(birthHeader)
+    let homeHeader = document.createElement("th")
+    homeHeader.innerHTML = "homeworld"
+    row2.appendChild(homeHeader)
+    // loop through characters in movie
     for(let i = 0; i < movie.characters.length; i++){
+        // promise to SW API
         fetch(movie.characters[i])
             .then(res=>res.json())
             .then(data=>renderChar(data))
@@ -49,15 +54,30 @@ function getChar(data){
 
 }
 
+// render character names, birth year and homeworld in cells
 function renderChar(data){
+    // insert name
     let char = data.result.properties
     let row = table.insertRow(rowCount)
     let nameData = row.insertCell(0)
     nameData.innerHTML = char.name
     
+    // insert birth year
     let birthData = row.insertCell(1)
     birthData.innerHTML = char.birth_year
-    row.appendChild(row)
     rowCount+=1
+
+    // promise to API for homeworld
+    fetch(char.homeworld)
+        .then(res=>res.json())
+        .then(data=>renderHome(row, data))
+    
+}
+
+// render homeworld from promise
+function renderHome(row, data){
+    let planet = data.result.properties.name
+    let homeData = row.insertCell(2)
+    homeData.innerHTML = planet
     
 }
